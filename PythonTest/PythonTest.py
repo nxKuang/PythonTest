ad9dccb878ad5f49e60d6e466cd9176d31822070
kuangningxiang <kuangningxiang@hotmail.com>
2017-06-10 21:20:27 +0800

    Test Python

---
+++ PythonTest/PythonTest.py
@@ -0,0 +1,19 @@
+﻿# Add your code here to run in your startup task
+from datetime import datetime
+import subprocess
+import os.path
+datestamp = datetime.now().strftime("%Y%m%d")
+game_path = "H:\Game\wow\WTF\Account\KUANGNX\\"
+archive_path = "H:\Game\wow\WTF\wow_account_archive\\"
+filename = archive_path+"KUANGNX_"+datestamp+".rar"
+if not os.path.isfile(filename):
+    subprocess.call(["rar","a","-r",filename,game_path])
+
+def main():
+    for i in range(10000):
+        s = make_dot_string(i)
+        print(s)
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
+
